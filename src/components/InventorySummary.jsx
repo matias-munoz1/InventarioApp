afed162a6010ba9c27c5b2b22c19c730cc2df081
Matias <mmunozs1996@gmail.com>
2024-10-25 07:08:30 -0300

    fix 4

--- src/components/InventorySummary.jsx
+++ src/components/InventorySummary.jsx
@@ -1,36 +1,30 @@
 // InventorySummary.jsx
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
 import { Table, Container, Spinner, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import pdfMake from 'pdfmake/build/pdfmake';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-pdfMake.vfs = pdfFonts.pdfMake.vfs; //
+import html2pdf from 'html2pdf.js'; // Importación de html2pdf
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import { AuthContext } from './AuthContext';
 
-
-
 function InventorySummary() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-
   const navigate = useNavigate();
-  const { userRole } = useContext(AuthContext); // Obtener el rol del usuario
+  const { userRole } = useContext(AuthContext);
+  const summaryRef = useRef(); // Referencia para el contenedor HTML
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Obtener productos
         const productsSnapshot = await getDocs(collection(db, 'task'));
         const fetchedProducts = productsSnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
         setProducts(fetchedProducts);
-
         setLoading(false);
       } catch (err) {
         console.error('Error al obtener los datos:', err);
@@ -41,77 +35,14 @@ function InventorySummary() {
   }, []);
 
   const generatePDF = () => {
-    const docDefinition = {
-      content: [
-        { text: 'Resumen de Inventario', style: 'header' },
-        {
-          table: {
-            headerRows: 1,
-            widths: ['*', 'auto', '*', '*'],
-            body: [
-              [
-                { text: 'Nombre del Producto', style: 'tableHeader' },
-                { text: 'Stock', style: 'tableHeader' },
-                { text: 'Última Modificación', style: 'tableHeader' },
-                { text: 'Cambio Generado', style: 'tableHeader' },
-              ],
-              ...products.map((product) => {
-                const lastModifiedDate = product.lastModified
-                  ? product.lastModified.toDate().toLocaleString()
-                  : 'Sin datos';
-
-                let cambioGenerado = product.lastChangeType || 'Sin cambios';
-
-                if (
-                  product.lastStockChangeAmount !== undefined &&
-                  product.lastStockChangeAmount !== 0
-                ) {
-                  const stockChangeSign = product.lastStockChangeAmount > 0 ? '+' : '';
-                  cambioGenerado += ` (${stockChangeSign}${product.lastStockChangeAmount})`;
-                }
-
-                return [
-                  { text: product.title, style: 'tableData' },
-                  {
-                    text: product.stock.toString(),
-                    style: 'tableData',
-                    alignment: 'center',
-                  },
-                  { text: lastModifiedDate, style: 'tableData' },
-                  { text: cambioGenerado, style: 'tableData' },
-                ];
-              }),
-            ],
-          },
-          layout: 'lightHorizontalLines',
-        },
-      ],
-      styles: {
-        header: {
-          fontSize: 18,
-          bold: true,
-          alignment: 'center',
-          margin: [0, 0, 0, 20],
-        },
-        tableHeader: {
-          bold: true,
-          fontSize: 12,
-          color: 'white',
-          fillColor: '#2d4154',
-          alignment: 'center',
-        },
-        tableData: {
-          fontSize: 10,
-        },
-      },
-      defaultStyle: {
-        font: 'Roboto',
-      },
-      pageSize: 'A4',
-      pageMargins: [40, 60, 40, 60],
+    const element = summaryRef.current;
+    const options = {
+      margin: 10,
+      filename: 'Resumen_de_Inventario.pdf',
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
     };
-
-    pdfMake.createPdf(docDefinition).download('Resumen_de_Inventario.pdf');
+    html2pdf().set(options).from(element).save();
   };
 
   if (loading) {
@@ -126,61 +57,63 @@ function InventorySummary() {
 
   return (
     <Container className="mt-5">
-      {/* Encabezado */}
-      <header className="d-flex justify-content-between align-items-center mb-4 p-3 bg-light shadow-sm rounded">
-        <div className="d-flex align-items-center">
-          <img src="/logo.png" alt="Logo Empresa" className="logo" />
-          <h1 className="text-primary fw-bold mb-0 ms-3">Resumen de Inventario</h1>
-        </div>
-        <div>
-          {userRole === 'owner' && (
-            <Button variant="primary" className="me-2" onClick={generatePDF}>
-              Generar PDF
+      <div ref={summaryRef}>
+        {/* Encabezado */}
+        <header className="d-flex justify-content-between align-items-center mb-4 p-3 bg-light shadow-sm rounded">
+          <div className="d-flex align-items-center">
+            <img src="/logo.png" alt="Logo Empresa" className="logo" />
+            <h1 className="text-primary fw-bold mb-0 ms-3">Resumen de Inventario</h1>
+          </div>
+          <div>
+            {userRole === 'owner' && (
+              <Button variant="primary" className="me-2" onClick={generatePDF}>
+                Generar PDF
+              </Button>
+            )}
+            <Button variant="secondary" onClick={() => navigate('/')}>
+              &larr; Volver al Inventario
             </Button>
-          )}
-          <Button variant="secondary" onClick={() => navigate('/')}>
-            &larr; Volver al Inventario
-          </Button>
-        </div>
-      </header>
+          </div>
+        </header>
 
-      {/* Tabla de Productos */}
-      <Table striped bordered hover responsive>
-        <thead>
-          <tr>
-            <th>Nombre del Producto</th>
-            <th>Stock</th>
-            <th>Última Modificación</th>
-            <th>Cambio Generado</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => {
-            const lastModifiedDate = product.lastModified
-              ? product.lastModified.toDate().toLocaleString()
-              : 'Sin datos';
+        {/* Tabla de Productos */}
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Nombre del Producto</th>
+              <th>Stock</th>
+              <th>Última Modificación</th>
+              <th>Cambio Generado</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => {
+              const lastModifiedDate = product.lastModified
+                ? product.lastModified.toDate().toLocaleString()
+                : 'Sin datos';
 
-            let cambioGenerado = product.lastChangeType || 'Sin cambios';
+              let cambioGenerado = product.lastChangeType || 'Sin cambios';
 
-            if (
-              product.lastStockChangeAmount !== undefined &&
-              product.lastStockChangeAmount !== 0
-            ) {
-              const stockChangeSign = product.lastStockChangeAmount > 0 ? '+' : '';
-              cambioGenerado += ` (${stockChangeSign}${product.lastStockChangeAmount})`;
-            }
+              if (
+                product.lastStockChangeAmount !== undefined &&
+                product.lastStockChangeAmount !== 0
+              ) {
+                const stockChangeSign = product.lastStockChangeAmount > 0 ? '+' : '';
+                cambioGenerado += ` (${stockChangeSign}${product.lastStockChangeAmount})`;
+              }
 
-            return (
-              <tr key={product.id}>
-                <td>{product.title}</td>
-                <td>{product.stock}</td>
-                <td>{lastModifiedDate}</td>
-                <td>{cambioGenerado}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+              return (
+                <tr key={product.id}>
+                  <td>{product.title}</td>
+                  <td>{product.stock}</td>
+                  <td>{lastModifiedDate}</td>
+                  <td>{cambioGenerado}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
     </Container>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['pdfmake/build/pdfmake', 'pdfmake/build/vfs_fonts'],
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Ajusta el límite del tamaño del chunk si es necesario.
+  },
+});
