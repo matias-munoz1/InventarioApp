3b0415e8b56669ab5c5eb849a8d938bcfe2dd613
Matias <mmunozs1996@gmail.com>
2024-10-25 06:34:26 -0300

    fix

--- src/components/InventorySummary.jsx
+++ src/components/InventorySummary.jsx
@@ -5,13 +5,13 @@ import { db } from '../firebase';
 import { Table, Container, Spinner, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import pdfMake from 'pdfmake/build/pdfmake';
-import { vfs } from 'pdfmake/build/vfs_fonts'; // Importación corregida
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs; //
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import { AuthContext } from './AuthContext';
 
-// Registrar las fuentes
-pdfMake.vfs = vfs;
+
 
 function InventorySummary() {
   const [products, setProducts] = useState([]);
